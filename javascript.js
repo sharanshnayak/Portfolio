7eae2ccaac166de37a8acce7e573bbcb95f2bd46
sharansh nayak <nayaksharansh@gmail.com>
2023-02-20 22:51:52 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+  strings: ["Developer", "Programmer", "Engineer", "Contributor", "Learner"],
+  typeSpeed: 120,
+  backSpeed: 100,
+  loop: true,
+});
+
+var typed = new Typed(".typing-2", {
+  strings: ["Developer", "Programmer", "Engineer", "Contributor", "Learner"],
+  typeSpeed: 120,
+  backSpeed: 100,
+  loop: true,
+});
